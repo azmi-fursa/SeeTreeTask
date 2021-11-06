a370b4e78e1629ad93d5a3b333914d93efdd168a
azmi-fursa <91056755+azmi-fursa@users.noreply.github.com>
2021-11-06 20:58:20 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,41 +3,61 @@ from flask import render_template
 import numpy as np
 from PIL import Image
 
+database = {"IMAGE_FILE_NAME": { }}
 
 #function to read and turn RGB image to grayscale image (if the image is available)
-def readImageFromFileURL(imageFileURL):
-    check_response = requests.get(imageFileURL, stream=True).raw
+def try_open_image(imageFileURL):
+    is_valid = requests.get(imageFileURL, stream=True).raw
     try:
-        img1 = Image.open(check_response)
-    except:
-        return render_template('Error.html', msg='Bad image file name!')
-    return img1.convert('L')
+        img = Image.open(is_valid).convert('L')
+    except Exception:
+        return "error"
+    return img
 
+    
 def calc_pixel_features(name, function, numpy_grayscale):    
+    if name in database:
+        if function in database[name]:
+            statement = 'The ' + function + ' value of the image ('+name+') is : ' + database[name][function]
+            return render_template('Statistics.html', value=str(statement), name=name)
     percent = function[1:]
     if function == 'min':
         value = str(np.min(numpy_grayscale))
         statement = 'Minimum value of (' + name + ') is: ' + value
-        return statement
+        return statement,value
     
     elif function == 'max':
         value = str(np.max(numpy_grayscale))
         statement = 'Maximum value of (' + name + ') is: ' + value
-        return statement
+        return statement,value
     
     elif function == 'mean':
         value = str(np.mean(numpy_grayscale))
         statement = 'Mean value of (' + name + ') is: ' + value
-        return statement
+        return statement,value
     
     elif function == 'median':
         value = str(np.median(numpy_grayscale))
         statement = 'Minimum value of (' + name + ') is: ' + value
-        return statement
+        return statement,value
     
     elif function[0] == 'p' and percent.isnumeric() and int(percent) in range(0,101):
         value = str(np.percentile(numpy_grayscale, int(percent)))
         statement = 'The ' + str(percent) + '% percentile value of the image (' + name + ') is : ' + value
-        return statement
+        return statement,value
     else:
-        return "none"
+        return "none","none"
+
+
+def redundant_calculations(name,function):
+    if name in database:
+        if function in database[name]:
+            return database[name][function]
+    return "better luck next time"
+
+
+def update_database(name,function,value):
+    if name not in database:
+        database.update({name: {function: str(value)}})
+    elif function not in database[name]:
+        database[name].update({function: str(value)})
